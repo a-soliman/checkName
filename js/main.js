6073bda36d0530397d77c6bb8cf63ce9e3270053
Ahmed Soliman <ahmedanwar@mail.com>
2016-06-20 00:20:35 -0700

    programmed the for and if else loop

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ var text = "hey this is the gggg ahmed said he wouldnt come but he other\
             ahmed will be there";
 var myName = "ahmed";
 var hits = [];
+
+// for loop
+for( i = 0; i < text.length; i ++ ) {
+    // if statment to check the first letter
+    if ( text[i] === myName[0] ) {
+        for( j = i; j < i+myName.length; j++ ) {
+           hits.push(myName)
+           }
+        }
+    }
