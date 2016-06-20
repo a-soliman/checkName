a3fa6c490ff86fcbf7d77acb81a772e4cac2538c
Ahmed Soliman <ahmedanwar@mail.com>
2016-06-20 00:19:02 -0700

    declared the vars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Check my name tool
+
+/*jshint multistr:true */
+
+//declaring the vars
+var text = "hey this is the gggg ahmed said he wouldnt come but he other\
+            ahmed will be there";
+var myName = "ahmed";
+var hits = [];
